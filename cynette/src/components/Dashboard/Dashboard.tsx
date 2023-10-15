4b0577a7c9c1d15233b1fc197009cf0916da0c62
Kaygio Aguero <kaygioaguero@gmail.com>
2023-10-15 07:51:28 +0000

    feat: use state to manage the theme mode

--- cynette/src/components/Dashboard/Dashboard.tsx
+++ cynette/src/components/Dashboard/Dashboard.tsx
@@ -1,16 +1,22 @@
+import { useState } from 'react';
 import PageHandler from './PageHandler';
 import { Outlet } from 'react-router-dom';
 
 const Dashboard = () => {
+  const { theme, setTheme} = useState('');
+
+  const toggleDisplayMode = () => { 
+    setTheme( (theme: string) => (theme === 'light' ? 'dark' : 'light'))
+  }
   return (
-    <>
+    <div id={theme}>
       <PageHandler header>
       <div>
         <Outlet />
       </div>
       </PageHandler>
    
-    </>
+    </div>
   );
 };
 
