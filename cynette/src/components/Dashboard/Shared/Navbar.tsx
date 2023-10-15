3b3d3191440833b1078b8eacf72956e14eb62883
Kaygio Aguero <kaygioaguero@gmail.com>
2023-10-15 12:10:29 +0000

    feat: set active state and hover state

--- cynette/src/components/Dashboard/Shared/Navbar.tsx
+++ cynette/src/components/Dashboard/Shared/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import {  NavLink } from 'react-router-dom';
 import { ThemeContext } from '../ThemeContext';
 
 import ThemeSwitch from './ThemeSwitch';
@@ -10,11 +10,11 @@ function Navbar() {
       <div className='ml-5 m-auto hover:cursor-pointer'>
         <img src='' alt='company icon' />
       </div>
-      <ul className='flex gap-8 mr-5 '>
-        <li className='m-auto hover:cursor-pointer'><Link to="/Home">Home</Link></li>
-        <li className='m-auto hover:cursor-pointer'><Link to="/AboutUs">About</Link></li>
-        <li className='m-auto hover:cursor-pointer'><Link to="/ContactUs">Contact Us</Link></li>
-        <li className='m-auto hover:cursor-pointer'><Link to="/Blog">Blog</Link></li>
+      <ul className='flex gap-8 mr-5'>
+        <li className='m-auto hover:cursor-pointer'><NavLink to="/Home" className='hover:outline hover:outline-blue-100 hover:rounded-lg hover: p-2'>Home</NavLink></li>
+        <li className='m-auto hover:cursor-pointer'><NavLink to="/AboutUs" className='hover:outline hover:outline-blue-100 hover:rounded-lg hover: p-2'>About</NavLink></li>
+        <li className='m-auto hover:cursor-pointer'><NavLink to="/ContactUs" className='hover:outline hover:outline-blue-100 hover:rounded-lg hover: p-2'>Contact Us</NavLink></li>
+        <li className='m-auto hover:cursor-pointer'><NavLink to="/Blog" className='hover:outline hover:outline-blue-100 hover:rounded-lg hover: p-2'>Blog</NavLink></li>
         <div className='flex before:border before:border-solid before:border-red-400'>
           <label htmlFor='' className='ml-4 hover:cursor-pointer' onClick={themeValue.toggleDisplayMode}>
             <ThemeSwitch value = {themeValue.theme}/>
