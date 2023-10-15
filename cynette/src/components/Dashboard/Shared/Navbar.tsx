fcf4223c15d32b2a8f07d63e4a248c4f51e4cabe
Kaygio Aguero <kaygioaguero@gmail.com>
2023-10-15 11:09:03 +0000

    feat: add theme switch and hover state

--- cynette/src/components/Dashboard/Shared/Navbar.tsx
+++ cynette/src/components/Dashboard/Shared/Navbar.tsx
@@ -2,21 +2,22 @@ import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { ThemeContext } from '../ThemeContext';
 
+import ThemeSwitch from './ThemeSwitch';
 function Navbar() {
   const themeValue = useContext(ThemeContext)
   return (
     <nav className={`${themeValue.theme === 'dark' ? 'navbar' : 'navbar'} flex flex-row  justify-between py-4`}>
-      <div className='ml-5'>
+      <div className='ml-5 m-auto hover:cursor-pointer'>
         <img src='' alt='company icon' />
       </div>
       <ul className='flex gap-8 mr-5 '>
-        <li><Link to="/Home">Home</Link></li>
-        <li><Link to="/AboutUs">About</Link></li>
-        <li><Link to="/ContactUs">Contact Us</Link></li>
-        <li><Link to="/Blog">Blog</Link></li>
+        <li className='m-auto hover:cursor-pointer'><Link to="/Home">Home</Link></li>
+        <li className='m-auto hover:cursor-pointer'><Link to="/AboutUs">About</Link></li>
+        <li className='m-auto hover:cursor-pointer'><Link to="/ContactUs">Contact Us</Link></li>
+        <li className='m-auto hover:cursor-pointer'><Link to="/Blog">Blog</Link></li>
         <div className='flex before:border before:border-solid before:border-red-400'>
-          <label htmlFor='' className='ml-4' onClick={themeValue.toggleDisplayMode}>
-            mode
+          <label htmlFor='' className='ml-4 hover:cursor-pointer' onClick={themeValue.toggleDisplayMode}>
+            <ThemeSwitch value = {themeValue.theme}/>
           </label>
         </div>
       </ul>
